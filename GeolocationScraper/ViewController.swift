128d6048ff7005236f15aef65a7b89f346bf23e3
frommwonderland <daniel.illner@outlook.de>
2019-11-04 14:08:00 +0200

    Update

--- GeolocationScraper/ViewController.swift
+++ GeolocationScraper/ViewController.swift
@@ -7,22 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager:CLLocationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
+
+
     @IBAction func startTapped(_ sender: Any) {
         print("started location process.")
+
+        //locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
-    
-    
+
+
     @IBAction func endTapped(_ sender: Any) {
+      locationManager.stopUpdatingLocation()
     }
-    
+
 
 }
-
