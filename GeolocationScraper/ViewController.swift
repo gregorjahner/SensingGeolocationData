c11d3a7169f5e4ec17913261205b7a3908be514d
frommwonderland <daniel.illner@outlook.de>
2019-11-09 15:03:34 +0200

    Update

--- GeolocationScraper/ViewController.swift
+++ GeolocationScraper/ViewController.swift
@@ -59,20 +59,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MFMailCompose
       let mailComposeViewController = configuredMailComposeViewController()
 
 
-
+      let attachmentData = csvText.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
       //let url = URL(fileURLWithPath: fileURL)
       //let url = NSBundle.mainBundle().pathForResource(fileName, ofType: "csv")
       //let url = NSBundle.mainBundle().pathForResource(fileURL, ofType: "csv")
-      let bundle = Bundle.main
-      let url = bundle.path(forResource: "locations", ofType: "csv")
-      print(url)
+      //let bundle = Bundle.main
+      //let url = bundle.path(forResource: "locations", ofType: "csv")
+      //print(url)
 
       do {
         //let attachmentData = try Data(contentsOf: url)
         //let attachmentData = Data(contentsOfFile: fileURL)
-        let attachmentData = Data(contentsOfFile: url)
+        //let attachmentData = Data(contentsOfFile: url)
         print("Test Attachment")
-        mailComposeViewController.addAttachmentData(attachmentData, mimeType: "text/csv", fileName: "locations.csv")
+        mailComposeViewController.addAttachmentData(attachmentData!, mimeType: "text/csv", fileName: "location.csv")
+        //mailComposeViewController.addAttachmentData(attachmentData, mimeType: "text/csv", fileName: "locations.csv")
         print("test test")
         mailComposeViewController.mailComposeDelegate = self
         self.present(mailComposeViewController, animated: true, completion: nil)
