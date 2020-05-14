4e8b2b50385eb838ccf25c1a270374242ccc1837
Gregor Jahner <gregor.jahner@gmx.de>
2020-05-14 16:52:12 +0200

    Update GH

--- rhinoConv.py
+++ rhinoConv.py
@@ -1,12 +1,22 @@
+"""Provides a scripting component.
+    Inputs:
+        x: The x script variable
+        y: The y script variable
+    Output:
+        a: The a output variable"""
+
+__author__ = "gregorjahner"
+
 # Import points from a text file
 import rhinoscriptsyntax as rs
 import csv
-import pandas as pd
+import ghpythonlib.treehelpers as th
+#import pandas as pd
 
 def calTotalNumberOfSpots():
-    df = pd.read_csv("top_spots.csv", usecols = ['0'])
-    return df['0'].sum()
-
+    #df = pd.read_csv("top_spots.csv", usecols = ['0'])
+    #return df['0'].sum()
+    pass
 
 
 def CSVlist():
@@ -15,6 +25,7 @@ def CSVlist():
     filename = rs.OpenFileName("top_spots", filter)
     if not filename: return
 
+    tmplist = list()
     with open(filename) as csvfile:
     #with open('top_spots.csv') as csvfile:
         reader = csv.reader(csvfile)
@@ -23,7 +34,7 @@ def CSVlist():
         for row in reader:
             if header_idx == True:
                 header_idx = False
-                print(row)
+                #print(row)
                 continue
 
             lat = float(row[0])
@@ -31,9 +42,17 @@ def CSVlist():
             alt = float(row[2])
             print(lat, long, alt)
 
-            rs.AddPoint(x,y,z)
+            rs.AddPoint(lat, long, alt)
+            tmplist.append([lat, long, alt])
+    print(tmplist)
+    b = th.list_to_tree(tmplist)
+    return b
 
-
+"""
 if( __name__ == "__main__" ):
     CSVlist()
     #calTotalNumberOfSpots()
+
+"""
+b = CSVlist()
+
